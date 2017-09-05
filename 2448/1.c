ab5e44263a7941bf7848798664de71b597857eb5
김성훈 <gimseonghun@gim-yeongjaeui-MacBook-Air.local>
2017-09-05 16:27:33 +0900

    2017-09-05

--- 2448/1.c
+++ 2448/1.c
@@ -1,68 +1,17 @@
-/**
- * @author [KSH-Code]
- * @email [tjdgnsqn133@gmail.com]
- * @create date 2017-08-27 08:44:59
- * @modify date 2017-08-27 08:44:59
- * @desc [https://www.acmicpc.net/problem/2448]
-*/
-#include <stdio.h>
-void changeStar(int *n)
-{
-    if (*n == 1)
-    {
-        *n = 2;
-    }
-    else if (*n == 2)
-    {
-        *n = 5;
-    }
-    else
-    {
-        *n = 1;
-    }
-}
-void printStar(int n)
-{
-    switch (n)
-    {
-    case 1:
-        printf("*");
-        break;
-    case 2:
-        printf("* *");
-        break;
-    case 5:
-        printf("*****");
-        break;
-    }
-}
-int main(void)
-{
-    int length, i, j, star = 1, klength,k, t = 1, tempt;
-    scanf("%d", &length);
-    for (i = 1; i <= length; i++)
-    {
-        if (length > i)
-        {
-            for (j = length - 1; j >= i; j--)
-                printf(" ");
+
+#include<stdio.h>
+int n, a[3072][6146];
+int main() {
+    scanf("%d", &n);
+    a[0][n - 1] = a[1][n - 2] = 
+    a[1][n] = a[2][n - 3] = a[2][n - 2] = 
+    a[2][n - 1] = a[2][n] = a[2][n + 1] = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 2 * n - 1; j++) {
+            if (i > 2) a[i][j] = (j>2 && a[i - 3][j - 3]) ^ a[i - 3][j + 3];
+            putchar(" *"[a[i][j]]);
         }
-        if (i % 3 == 1 && i > 1)
-        {
-            klength = (i - 1) / 3;
-            for (k=1;)
-            {
-            }
-            t *= 2;
-        }
-        printStar(star);
-        if (length > i)
-        {
-            for (j = length; j >= i; j--)
-                printf(" ");
-            printf("\n");
-        }
-        changeStar(&star);
+        puts("");
     }
     return 0;
 }
