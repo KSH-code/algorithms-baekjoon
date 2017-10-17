ccdc17d24c9d282717e5bb76854e50687a0cc220
KSH-Code <tjdgnsqn133@gmail.com>
2017-10-17 13:48:19 +0900

    다익스트라, 고양이 풀이

--- 1016/Main.java
+++ 1016/Main.java
@@ -6,34 +6,24 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         long N,M;
         String str1[] = br.readLine().split(" ");
-        ArrayList<Integer> I_list = new ArrayList<>();
+        boolean pow[] = new boolean[1000000+1];
         double sq;
         N = Long.parseLong(str1[0]);
         M = Long.parseLong(str1[1]);
-        for(int i = 2;i<=1000000;i++){
-            sq = Math.sqrt(i);
-            boolean check = false;
-            for(int j = 2;j<=sq;j++){
-                if(i%j==0){
-                    check = true;
-                    break;
-                }
-            }
-            if(!check){
-                I_list.add(i);
-            }
+        Arrays.fill(pow, true);
+        for(int i = 2;i<=1000;i++){
+            if(pow[i]){
+				for(int j = i*i; j<=1000000; j+=i) pow[j]=false;
+			}
         }
         long sum = 0;
         for(long i = N;i<=M;i++){
-            for(int j = 0;j<I_list.size();j++){
-                int temp = I_list.get(j);
-                long pow = (long)(temp*temp);
-                if(pow>i)
-                    break;
-                if(i%pow==0){
-                    sum++;
-                    break;
-                }
+            for(int j = 2;j<=1000000;j++){
+                if(pow[j])
+                    if(i%j==0){
+                        sum++;
+                        break;
+                    }
             }
         }
         System.out.println(M-N-sum+1);
