909ab495fa6cdca54e004bc601d116bf4132fcd9
KSH-Code <tjdgnsqn133@gmail.com>
2017-10-18 23:04:08 +0900

    타일채우기 DP

--- 14852/Main.java
+++ 14852/Main.java
@@ -3,30 +3,25 @@ import java.util.*;
 
 /**
  * BOJ 14852 타일채우기 3
+ * https://gist.github.com/KSH-code/aaf918ce45a1ca3da9705f4a83aae78b
  */
 public class Main{
     private static final int MOD = (int)(1e+9)+7;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        int dp[] = new int[N+1];
-        int DP[] = new int[N+1];
-        DP[0] = 0;
-        DP[1] = 1;
-        dp[1] = 1;
-        if(N>1){
-            DP[2] = 2;
-            dp[2] = 2;
+        
+        long dp[] = new long[N+10];
+        long dp2[] = new long[N+10];
+        dp[1]=2;
+        dp[2]=7;
+        dp[3]=22;
+        dp2[3]=1;
+        for(int i = 4; i<=N; i++){
+            dp2[i] = (dp2[i-1]+dp[i-3])%MOD;
+            dp[i] = (dp[i-2]*3+dp[i-1]*2+dp2[i]*2)%MOD; 
         }
-        for(int i = 3; i<=N; i++){
-            DP[i] = (DP[i - 1] + DP[i - 2]) % MOD;
-            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
-        }
-        bw.write(String.valueOf((dp[N]+DP[N])%MOD));
-
-        bw.flush();
-
+        System.out.println(dp[N]);
     }
 }
