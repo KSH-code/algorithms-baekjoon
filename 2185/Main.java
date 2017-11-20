2936c0692bfd8d5c0e3ccf8b47c60ca5573c82e1
KSH-Code <tjdgnsqn133@gmail.com>
2017-11-20 22:10:16 +0900

    나중에 풀어볼 것

--- 2185/Main.java
+++ 2185/Main.java
@@ -1,57 +1,24 @@
 import java.util.*;
+import java.io.*;
 /**
  * BOJ 백준온라인져지 2185 직사각형의 합집합 풀이
  * 아직 코드는 안짬
  * 솔루션 : 
- * 1. 플레인스위핑으로 사각형들을 다 합친다.
- * 2. 라인을 그어서 확인하고 길이를 더해준다.
+ * 1. (0,0) (N,0) 부터 (0,M) (N,M) 까지 둘레의 가로길이 찾고
+ * 2. (0,0) (0,M) 부터 (N,0) (N,M) 까지 둘레의 세로길이를 찾자
  * 참고 : https://blog.sisobus.com/2014/02/plane-sweeping.html#.WgwlJbSFjBI
+ *       http://codedoc.tistory.com/421
  */
 public class Main {
     public static void main(String args[]) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-
-        int ans = 0;
-
-        ArrayList<rect> d = new ArrayList<>();
-        ArrayList<Integer> x = new ArrayList<>();
-        ArrayList<Integer> y = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            rect t = new rect();
-            t.x = scanner.nextInt() + 10001;
-            t.y = scanner.nextInt() + 10001;
-            int x2 = scanner.nextInt() + 10001;
-            int y2 = scanner.nextInt() + 10001;
-            t.width = x2 - t.x;
-            t.height = y2 - t.y;
-
-            d.add(t);
-
-            x.add(d.get(i).x);
-            x.add(x2);
-            y.add(d.get(i).y);
-            y.add(y2);
-        }
-
-        Collections.sort(x);
-        Collections.sort(y);
-
-        for (int i = 0; i < x.size() - 1; i++)
-            for (int j = 0; j < y.size() - 1; j++)
-                for (int k = 0; k < n; k++)
-                    if (x.get(i) >= d.get(k).x && x.get(i + 1) <= d.get(k).x + d.get(k).width &&
-                            y.get(j) >= d.get(k).y && y.get(j + 1) <= d.get(k).y + d.get(k).height) {
-                        ans += (x.get(i + 1) - x.get(i)) * (y.get(j + 1) - y.get(j));
-                        break;
-                    }
-
-        System.out.println(ans);
+        
     }
 
-    static class rect {
-        int x, y, width, height;
-        rect(){}
+    static class Rect {
+        int x1, y1, x2, y2;
+        public Rect(int a,int b,int c, int d){
+            x1 = a, y1 = b;
+            x2 = c, y2 = d;
+        }
     }
 }
