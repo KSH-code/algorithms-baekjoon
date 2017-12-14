225af4bcf6da046bd8da656d742a4cf4596bcff5
KSH-Code <tjdgnsqn133@gmail.com>
2017-12-14 13:39:37 +0900

    BOJ 백준온라인져지 1456 거의 소수 풀이

--- 1456/Main.cpp
+++ 1456/Main.cpp
@@ -2,16 +2,28 @@
 /**
  * https://www.acmicpc.net/problem/1456
  * BOJ 백준온라인져지 1456 거의 소수 풀이
- * http://ksh-code.tistory.com/102
  */
 int main(){
     long long min, max;
     int count = 0;
     scanf("%lld%lld", &min, &max);
-    for(long long i = 2; i * i <= max; i++){
-        for(long long j = i * i; j <= max; j *= i){
-            count++;
+    bool *isPrimeNumber = new bool[(int)1e+7 + 1];
+    int *primeNumber = new int[(int)1e+7 + 1];
+    int cnt = 0;
+    for(long long i = 2; i * i <= max ; i++){
+        if(!isPrimeNumber[i]){
+            for(long long j = i * 2; j * j <= max; j += i) isPrimeNumber[j] = true;
+            primeNumber[cnt++] = i;
         }
     }
+    
+    for(int i = 0; i < cnt; i++){
+        long long n = primeNumber[i];
+        while(primeNumber[i] <= max / n){
+            if(primeNumber[i] * n >= min) count++;
+            n *= primeNumber[i];
+        } 
+    }
+
     printf("%d", count);
 }
