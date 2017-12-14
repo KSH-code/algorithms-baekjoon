047ac058004bb8c2a86d04406a18a01a7d048155
KSH-Code <tjdgnsqn133@gmail.com>
2017-12-14 10:59:18 +0900

    BOJ 백준온라인져지 14921 용액 합성하기 풀이

--- 14921/Main.cpp
+++ 14921/Main.cpp
@@ -16,31 +16,19 @@ int main(){
     int N;
     scanf("%d", &N);
     int *list = new int[N];
-    int negativeN = -1, positiveN = -1;
     for(int i = 0; i < N; i++){
         scanf("%d", &list[i]);
-        if(list[i] >= 0 && positiveN == -1){
-            positiveN = i;
-        }
-        if(list[i] < 0 && negativeN == -1){
-            negativeN = i;
-        }
     }
     int maximum = 1000000000, l = 0, r = N - 1;
-    bool left = false;
-    if(positiveN == -1) maximum = list[N - 1] + list[N - 2];
-    else if(negativeN == -1) maximum = list[0] + list[1];
-    else{
-        do{
-            if(abs(maximum) > abs(list[l] + list[r])){
-                maximum = list[l] + list[r];
-                if(positiveN == r) l++;
-                else r--;
-            }else{
-                if(abs(list[l] + list[r]) > abs(list[l] + list[r - 1])) r--;
-                else l++;
-            }
-        }while(l < positiveN && r >= positiveN);
-    }
+    do{
+        if(abs(maximum) > abs(list[l] + list[r])){
+            maximum = list[l] + list[r];
+        }
+        if(abs(list[l] + list[r]) > abs(list[l] + list[r - 1])){
+            r--;
+        }else{
+            l++;
+        }
+    }while(l < r);
     printf("%d", maximum);
 }
