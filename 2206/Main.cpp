5811c58790d985cd96787b6a30e732dbc1dd5bd4
KSH-Code <tjdgnsqn133@gmail.com>
2017-12-15 13:44:45 +0900

    BOJ 백준온라인져지 2206 벽 부수고 이동하기 풀이

--- 2206/Main.cpp
+++ 2206/Main.cpp
@@ -2,12 +2,19 @@
 #include <queue>
 /**
  * https://www.acmicpc.net/problem/2206
- * BOJ 백준온라인져지 2206 미로 탈출 풀이
+ * BOJ 백준온라인져지 2206 벽 부수고 이동하기 풀이
  */
 #define INF 99999999
 using namespace std;
 int xxxx[] = {0, 1, 0 , -1};
 int yyyy[] = {-1, 0, 1 , 0};
+
+class coordinate{
+    public:
+        int x, y;
+        bool z;
+        coordinate(int _x, int _y, bool _z) :x(_x), y(_y), z(_z){}
+};
 
 int min(int a, int b){
     return a < b ? a : b;
@@ -24,7 +31,7 @@ int main(){
         map[i] = new int[columnSize];
         isVisited[i] = new bool[columnSize];
         for(int j = 1; j < columnSize; j++){
-            resultArray[i][j] = new int[3];
+            resultArray[i][j] = new int[2];
             char temp;
             scanf("%c", &temp);
             if(temp < '0' || temp > '9'){
@@ -34,31 +41,29 @@ int main(){
             resultArray[i][j][0] = resultArray[i][j][1] = INF, map[i][j] = temp - 48;
         }
     }
-
     int startX = 1, startY = 1;
     int endX = rowSize - 1, endY = columnSize - 1;
 
     resultArray[startX][startY][0] = 1;
-    queue<pair<int, int> > queue;
+    queue<coordinate> queue;
+    queue.push(coordinate(1, 1, false));
 
-    queue.push(make_pair(startX, startY));
 
-
-    while(queue.size()){
-        pair<int, int> polledData = queue.front();
+    while(!queue.empty()){
+        coordinate polledData = queue.front();
         queue.pop();
-        int x = polledData.first;
-        int y = polledData.second;
-        int z = resultArray[x][y][2];
+        int x = polledData.x;
+        int y = polledData.y;
+        bool z = polledData.z;
         int moveCount = resultArray[x][y][z] + 1;
         for(int i = 0 ; i < 4; i++){
             int newX = xxxx[i] + x;
             int newY = yyyy[i] + y;
             if(newX <= 0 || newX >= rowSize || newY <= 0 || newY >= columnSize) continue;
             if(map[newX][newY] == 0 && moveCount < resultArray[newX][newY][z]){
-                queue.push(make_pair(newX, newY)), resultArray[newX][newY][z] = moveCount;
-            }else if(map[newX][newY] == 1 && z == 0 && moveCount < resultArray[newX][newY][1]){
-                queue.push(make_pair(newX, newY)), resultArray[newX][newY][1] = moveCount,
+                queue.push(coordinate(newX, newY, z)), resultArray[newX][newY][z] = moveCount;
+            }else if(map[newX][newY] == 1 && !z && moveCount < resultArray[newX][newY][1]){
+                queue.push(coordinate(newX, newY, 1)), resultArray[newX][newY][1] = moveCount,
                 resultArray[newX][newY][2] = 1;
             }
         }
